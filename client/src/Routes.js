2da8665d4cba70acc5ea0a77c509750c5e5b5a71
suprsidr <suprsidr@gmail.com>
2017-10-30 13:02:28 -0400

    signup working

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import AppliedRoute from "./components/AppliedRoutes";
 import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 
-export default () => (
+export default ({ childProps }) => (
   <Switch>
-    <Route path="/" exact component={Home} />
+    <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    {/* Finally, catch all unmatched routes */}
+    <Route component={NotFound} />
   </Switch>
 );
---
+++ client/src/config.js
@@ -0,0 +1,6 @@
+export default {
+  cognito: {
+    USER_POOL_ID: "us-east-1_jv0VhXbou",
+    APP_CLIENT_ID: "43beq9j60sjg12qugje07k96v"
+  }
+};
